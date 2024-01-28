aa5c440f603fa72d07ed472843f24710ff357c86
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 15:39:44 +0800

    Update renew-ws.sh

--- janda/renew-ws.sh
+++ janda/renew-ws.sh
@@ -52,7 +52,7 @@ exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
 sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json
+sed -i "s/#vmg $user $exp/#vmg $user $exp4/g" /etc/xray/config.json
 systemctl restart xray.service
 service cron restart
 clear
