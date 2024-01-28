4060a6ef1f855ba51c58210a4fdfe9f88a16785a
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 15:43:04 +0800

    Update insshws.sh

--- baku/insshws.sh
+++ baku/insshws.sh
@@ -4,18 +4,12 @@
 cd
 
 #Install Script Websocket-SSH Python
-#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/galat41/bkn/main/baku/openssh-socket.py
 wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/galat41/bkn/main/baku/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/galat41/bkn/main/baku/ws-stunnel
 
 #izin permision
-#chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
-
-
-#System OpenSSH Websocket-SSH Python
-#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/galat41/bkn/main/baku/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
 
 #System Dropbear Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/galat41/bkn/main/baku/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
@@ -26,11 +20,6 @@ wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com
 #restart service
 #
 systemctl daemon-reload
-#Enable & Start & Restart ws-openssh service
-#systemctl enable ws-openssh.service
-#systemctl start ws-openssh.service
-#systemctl restart ws-openssh.service
-
 #Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
