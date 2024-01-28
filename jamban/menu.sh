3b43487b77ea02e96d0865381333ea5307b66d7a
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 13:57:01 +0800

    Update menu.sh

--- jamban/menu.sh
+++ jamban/menu.sh
@@ -4,10 +4,9 @@
 # Author  : JULAK BANTUR
 # (C) Copyright 2023-2024 By JULAK BANTUR
 # =========================================
-MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me)
 echo "Checking VPS"
-#########################
-
+clear
 # Color Validation
 DF='\e[39m'
 Bold='\e[1m'
@@ -21,6 +20,11 @@ cyan='\e[36m'
 Lred='\e[91m'
 Lgreen='\e[92m'
 Lyellow='\e[93m'
+BGreen='\e[1;32m'
+BYellow='\e[1;33m'
+BBlue='\e[1;34m'
+BPurple='\e[1;35m'
+BCyan='\e[1;36m'
 NC='\e[0m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
@@ -54,16 +58,21 @@ tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}
 dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')"
 umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
 tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
+# user
+Exp2=$"Lifetime"
+Name=$"Julak-Bantur"
 # Getting CPU Information
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
 cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
 cpu_usage+=" %"
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-WKT=$(curl -s ipinfo.io/timezone )
+#ISP=$(curl -s ipinfo.io/org?token=ce3da57536810d | cut -d " " -f 2-10 )
+#CITY=$(curl -s ipinfo.io/city?token=ce3da57536810d )
+#WKT=$(curl -s ipinfo.io/timezone?token=ce3da57536810d )
 DAY=$(date +%A)
 DATE=$(date +%m/%d/%Y)
-IPVPS=$(curl -s ipinfo.io/ip )
+DATE2=$(date -R | cut -d " " -f -5)
+IPVPS=$(curl -s ifconfig.me )
+LOC=$(curl -s ifconfig.co/country )
 cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
 cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
 freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
@@ -71,27 +80,43 @@ tram=$( free -m | awk 'NR==2 {print $2}' )
 uram=$( free -m | awk 'NR==2 {print $3}' )
 fram=$( free -m | awk 'NR==2 {print $4}' )
 clear 
-echo -e "\e[33m                                                            \e[0m"
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "                    • VPS INFO •                 "
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "                                                                                         "
-echo -e "\e[33m Operating System     \e[0m:  "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
-echo -e "\e[33m Total Amount Of RAM  \e[0m:  $tram MB"
-echo -e "\e[33m System Uptime        \e[0m:  $uptime "
-echo -e "\e[33m Isp Name             \e[0m:  $ISP"
-echo -e "\e[33m Domain               \e[0m:  $domain"	
-echo -e "\e[33m Ip Vps               \e[0m:  $IPVPS"	
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "                 • SCRIPT MENU •                 "
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " [\e[36m•1\e[0m] SSH Menu            [\e[36m•5\e[0m] System Menu"
-echo -e " [\e[36m•2\e[0m] Vmess Menu          [\e[36m•6\e[0m] Status Service"
-echo -e " [\e[36m•3\e[0m] Vless Menu          [\e[36m•7\e[0m] Clear RAM Cache"
-echo -e " [\e[36m•4\e[0m] Trojan Menu         [\e[36m•8\e[0m] Update AutoScript"                  
-echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e   " Press x or [ Ctrl+C ] • To-Exit-Script"
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e "\e[1;34m                      VPS INFO                    \e[0m"
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e "\e[1;32m OS            \e[0m: "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
+echo -e "\e[1;32m Uptime        \e[0m: $uptime"
+echo -e "\e[1;32m Public IP     \e[0m: $IPVPS"
+echo -e "\e[1;32m Country       \e[0m: $LOC"
+echo -e "\e[1;32m DOMAIN        \e[0m: $domain"	
+echo -e "\e[1;32m DATE & TIME   \e[0m: $DATE2"
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e "\e[1;34m                      RAM INFO                    \e[0m"
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e ""
+echo -e "\e[1;32m RAM USED   \e[0m: $uram MB"	
+echo -e "\e[1;32m RAM TOTAL  \e[0m: $tram MB"
+echo -e ""
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e "\e[1;34m                       MENU                       \e[0m"
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
 echo -e   ""
+echo -e "\e[1;36m 1 \e[0m: Menu SSH"
+echo -e "\e[1;36m 2 \e[0m: Menu Vmess"
+echo -e "\e[1;36m 3 \e[0m: Menu Vless"
+echo -e "\e[1;36m 4 \e[0m: Menu Trojan"
+echo -e "\e[1;36m 5 \e[0m: Menu Setting"
+echo -e "\e[1;36m 6 \e[0m: Status Service"
+echo -e "\e[1;36m 7 \e[0m: Clear RAM Cache"
+echo -e "\e[1;36m 8 \e[0m: Update Autoscript"
+echo -e "\e[1;36m 9 \e[0m: Reboot VPS"
+echo -e "\e[1;36m x \e[0m: Exit Script"
+echo -e   ""
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e "\e[1;32m Client Name \e[0m: $Name"
+echo -e "\e[1;32m Expired     \e[0m: $Exp2"
+echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e   ""
+echo -e "\e[1;36m --------------------wa.me/+6281250851741-------------------\e[0m"
 echo -e   ""
 read -p " Select menu :  "  opt
 echo -e   ""
@@ -104,6 +129,7 @@ case $opt in
 6) clear ; running ;;
 7) clear ; clearcache ;;
 8) clear ; menu-update ;;
+9) clear ; reboot ;;
 x) exit ;;
-*) menu ;;
+*) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
