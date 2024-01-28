76b1876333b630f20f37b04d9b19d7718110acd4
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 16:37:25 +0800

    Update menu-vless.sh

--- jamban/menu-vless.sh
+++ jamban/menu-vless.sh
@@ -12,11 +12,11 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 echo -e "\E[0;100;33m       • XRAY / VLESS MENU •         \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " [\e[36m01\e[0m] Create Vless Account "
-echo -e " [\e[36m02\e[0m] Trial Vless Account "
-echo -e " [\e[36m03\e[0m] Renew Vless Account "
+echo -e " [\e[36m02\e[0m] Renew Vless Account "
+echo -e " [\e[36m03\e[0m] Delete Vless Account "
 echo -e " [\e[36m04\e[0m] Cek User Login "
-echo -e " [\e[36m05\e[0m] Delete Vless Account "
-echo -e " [\e[36m06\e[0m] Check Member Vless Account "
+echo -e " [\e[36m05\e[0m] Check Member Vless Account "
+echo -e " [\e[36m06\e[0m] Trial Vless Account "
 echo -e " [\e[36m00\e[0m] Back To Menu "
 echo -e " [\e[36mxx\e[0m] Exit "
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -25,11 +25,11 @@ read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
 1) clear ; add-vless ; exit ;;
-2) clear ; trialvless ; exit ;;
-3) clear ; renew-vless ; exit ;;
+2) clear ; renew-vless ; exit ;;
+3) clear ; del-vless ; exit ;;
 4) clear ; cek-vless ; exit ;;
-5) clear ; del-vless ; exit ;;
-6) clear ; mem-vless ; exit ;;
+5) clear ; mem-vless ; exit ;;
+6) clear ; trialvless ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu-vless ;;
