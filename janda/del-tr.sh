1f3dcc5a82c4c1d3b8013c2f0971dc13c713419e
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 15:35:25 +0800

    Update del-tr.sh

--- janda/del-tr.sh
+++ janda/del-tr.sh
@@ -35,6 +35,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
     else
     exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
+    sed -i "/^#trg $user $exp/,/^},{/d" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -43,7 +44,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
     echo " Client Name : $user"
     echo " Expired On  : $exp"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo "Bagoes Vpn"
+    echo "Julak Bantur"
     read -n 1 -s -r -p "Press any key to back on menu"
     menu
     fi
