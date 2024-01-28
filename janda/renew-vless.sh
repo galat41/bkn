37bf27e2d726a144163dbda4501e3dff1c1637e1
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 15:38:53 +0800

    Update renew-vless.sh

--- janda/renew-vless.sh
+++ janda/renew-vless.sh
@@ -55,6 +55,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/config.json")
     exp3=$(($exp2 + $masaaktif))
     exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
     sed -i "/#& $user/c\#& $user $exp4" /etc/xray/config.json
+    sed -i "/#vlg $user/c\#vlg $user $exp4" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
