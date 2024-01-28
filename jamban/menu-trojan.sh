1b81be5f65ef7dfaa035b5153af7f8a5f4ccb9c6
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 16:35:38 +0800

    Update menu-trojan.sh

--- jamban/menu-trojan.sh
+++ jamban/menu-trojan.sh
@@ -12,11 +12,11 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 echo -e "\E[0;100;33m      • TROJAN GFW MENU •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " [\e[36m01\e[0m] Create Trojan Account "
-echo -e " [\e[36m02\e[0m] Trial Trojan Account "
-echo -e " [\e[36m03\e[0m] Renew Trojan Account "
+echo -e " [\e[36m02\e[0m] Renew Trojan Account "
+echo -e " [\e[36m03\e[0m] Delete Trojan Account "
 echo -e " [\e[36m04\e[0m] Cek User Login "
-echo -e " [\e[36m05\e[0m] Delete Trojan Account "
-echo -e " [\e[36m06\e[0m] Cek Member Trojan Account "
+echo -e " [\e[36m05\e[0m] Cek Member Trojan Account "
+echo -e " [\e[36m06\e[0m] Trial Trojan Account "
 echo -e " [\e[36m00\e[0m] Back To Menu "
 echo -e " [\e[36mxx\e[0m] Exit "
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -25,11 +25,11 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; add-tr ;;
-2) clear ; trialtrojan ;;
-3) clear ; renew-tr ;;
+2) clear ; renew-tr ;;
+3) clear ; del-tr ;;
 4) clear ; cek-tr ;;
-5) clear ; del-tr ;;
-6) clear ; mem-tr ;;
+5) clear ; mem-tr ;;
+6) clear ; trialtrojan ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo "Salah tekan Sayang" ; sleep 1 ; menu-trojan ;;
