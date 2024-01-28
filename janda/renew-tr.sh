0000edfb0029196ff573f9d5cd3e06912f7e0fdd
galat41 <157872742+galat41@users.noreply.github.com>
2024-01-28 15:37:58 +0800

    Update renew-tr.sh

--- janda/renew-tr.sh
+++ janda/renew-tr.sh
@@ -49,6 +49,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
     exp3=$(($exp2 + $masaaktif))
     exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
     sed -i "/#! $user/c\#! $user $exp4" /etc/xray/config.json
+    sed -i "/#trg $user/c\#trg $user $exp4" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
